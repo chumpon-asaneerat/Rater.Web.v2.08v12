41e1fb9f49b6287ee3a648eb7bb506e8a2ba228d
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-31 13:35:38 +0700

    Update 2020-03-31 #7 - Update server side cookie #13

--- routes/api/01.public/04.register/routes.js
+++ routes/api/01.public/04.register/routes.js
@@ -144,3 +144,84 @@ const init_routes = (svr) => {
 };
 
 module.exports.init_routes = exports.init_routes = init_routes;
+
+
+//#region exec/validate wrapper method - from summary report
+/*
+const exec = async (db, callback) => {
+    let ret;
+    let connected = await db.connect();
+    if (connected) {
+        ret = await callback();
+        await db.disconnect();
+    }
+    else {
+        ret = db.error(db.errorNumbers.CONNECT_ERROR, 'No database connection.');
+    }
+    return ret;
+}
+const validate = (db, data) => {
+    let result = data;
+    if (!result) {
+        result = db.error(db.errorNumbers.NO_DATA_ERROR, 'No data returns');
+    }
+    else {
+        result = checkForError(data);
+    }
+    return result;
+}
+const checkForError = (data) => {
+    let result = data;
+    if (result.out && result.out.errNum && result.out.errNum !== 0) {
+        result.errors.hasError = true;
+        result.errors.errNum = result.out.errNum;
+        result.errors.errMsg = result.out.errMsg;
+    }
+    return result;
+}
+*/
+//#endregion
+
+//#region Implement - Get - from summary report
+/*
+api.Get = class {
+    static prepare(req, res) {
+       let params = WebServer.parseReq(req).data;
+       rptAPI.question.checkLanguageId(params)
+       let customerId = secure.getCustomerId(req, res);
+       if (customerId) params.customerId = customerId;
+       params.deviceId = null;
+       params.userId = null;
+
+        return params;
+    }
+    static async call(db, params) { 
+        return await rptAPI.votesummary.load(db, params);
+    }
+    static parse(db, data, callback) {
+        let result = {
+            data: null,
+            errors: {
+                hasError: false,
+                errNum: 0,
+                errMsg: ''
+            },
+            out: {}
+        }
+        // set to result.
+        result.data = data;
+        callback(result);
+    }
+    static entry(req, res) {
+        let db = new sqldb();
+        let params = api.Get.prepare(req, res);
+        let fn = async () => { return api.Get.call(db, params); }
+        exec(db, fn).then(data => {
+            api.Get.parse(db, data, (result) => {
+                WebServer.sendJson(req, res, result);
+            });
+        })
+    }
+}
+*/
+//#endregion
