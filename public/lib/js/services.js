6ef3375d9f53d1911b1ab9dc56b6883cc271bcc9
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-23 17:37:39 +0700

    Update 2020-04-23 #2 - service.js - Update local storage class. - service.js - Implements UserPerference class. - service.js - Reimplement Language Service.

--- public/lib/js/services.js
+++ public/lib/js/services.js
@@ -39,6 +39,135 @@ window.events.doc = class {
 
 //#endregion
 
+//#region LocalStorage class
+
+class LocalStorage {
+    constructor() {
+        this._name = ''
+        this._data = null
+        this._ttl = 0;
+    };
+    //-- public methods.
+    checkExist() {
+        if (!this.data) {
+            this.data = this.getDefault();
+            this.save();
+        }
+    };
+    getDefault() { return {} };
+    save() {
+        if (!this._name) return; // no name specificed.
+        let ttl = (this._ttl) ? this._ttl : 0;
+        nlib.storage.set(this._name, this._data, { TTL: ttl }); // 1 days.
+    };
+    load() {
+        if (!this._name) return; // no name specificed.
+        let ttl = (this._ttl) ? this._ttl : 0;
+        this._data = nlib.storage.get(this._name);
+    };
+    //-- public properties.
+    get name() { return this._name; };
+    set name(value) { this._name = value; };
+    get data() { return this._data; };
+    set data(value) { this._data = value; };
+    get ttl() { return this._ttl; };
+    set ttl(value) { this._ttl = value; };
+};
+
+//#endregion
+
+//#region UserPerference class
+
+class UserPerference extends LocalStorage {
+    constructor() {
+        super();
+        this.name = 'uperf'
+        this.ttl = 0;
+        this._prefchanged = null;
+        this.load();
+        this.checkExist();
+    };
+    //-- public methods.
+    getDefault() { return { langId: 'EN' } };
+    load() { super.load(); };
+    save() { super.save(); };
+    //-- public properties.
+    get langId() {
+        if (!this.data) this.checkExist();
+        return this.data.langId;
+    };
+    set langId(value) {
+        if (!this.data) this.checkExist();
+        this.data.langId = value;
+    };
+};
+
+//#endregion
+
+//#region LanguageService class
+
+/** app language changed. */
+window.events.name.LanguageChanged = 'app:language:change';
+
+class LanguageService {
+    constructor() {
+        this.pref = new UserPerference();
+        this.pref.load(); // load once.
+        this.languages = [];
+        this.langId = LanguageService.defaultId;
+        this.current = LanguageService.defaultLang;
+        this.loaded = false;
+    }
+    getLanguages() {
+        let self = this;
+        let fn = (r) => {
+            let data = api.parse(r);
+            self.languages = (data && data.records) ? data.records : [];
+            self.change(self.pref.langId, true); // set langId from preference.
+            self.loaded = true;
+        }
+        XHR.get('/api/languages', { enable: true }, fn);
+    }
+    findLanguageIndex(langId) {
+        let ids = this.languages.map(lang => lang.langId);
+        return ids.indexOf(langId);
+    }
+    change(langId, forced) {
+        let newId = LanguageService.checkId(langId)
+        if (this.langId != newId || forced) {
+            this.langId = newId;
+            let idx = this.findLanguageIndex(newId);
+            this.current = (idx === -1) ? LanguageService.defaultLang : this.languages[idx];
+            // keep langid to storage.
+            this.pref.langId = this.current.langId;
+            this.pref.save();
+            // Raise event.
+            events.raise(events.name.LanguageChanged);
+        }
+    }
+    static get defaultId() { return 'EN' }
+    static get defaultLang() { 
+        return {
+            Description: "English",
+            Enabled: true,
+            SortOrder: 1,
+            flagId: "US",
+            langId: "EN"
+        } 
+    }
+    static checkId(langId) { 
+        return (langId) ? langId.toUpperCase() : LanguageService.defaultId
+    }
+}
+
+; (function () {
+    //console.log('Init language service...');
+    window.lang = window.lang || new LanguageService();
+    lang.getLanguages();
+})();
+
+//#endregion
+
 //#region ScreenService class
 
 /** app screen changed. */
