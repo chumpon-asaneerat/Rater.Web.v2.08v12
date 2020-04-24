356cb7c31fb11ec187c8103be4c5c39d0e23834b
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-24 07:11:19 +0700

    Update 2020-04-24 #13 - Fixed api routes #2

--- routes/api/03.customer/device/routes.js
+++ routes/api/03.customer/device/routes.js
@@ -6,15 +6,20 @@ const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
 
 const sqldb = require(path.join(nlib.paths.root, 'RaterWebv2x08r12.db'));
 const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
+const RaterStorage = require(path.join(rootPath, 'edl', 'rater-secure')).RaterStorage;
+
+const dbutils = require(path.join(rootPath, 'edl', 'utils', 'db-utils')).DbUtils;
+//const cookies = require(path.join(rootPath, 'edl', 'utils', 'cookie-utils')).CookieUtils;
+//const urls = require(path.join(rootPath, 'edl', 'utils', 'url-utils')).UrlUtils;
 
 const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
 const WebRouter = WebServer.WebRouter;
 const router = new WebRouter();
 
-//#endregion
+//const fs = require('fs')
+//const mkdirp = require('mkdirp')
 
-const fs = require('fs')
-const mkdirp = require('mkdirp')
+//#endregion
 
 //#region exec/validate wrapper method
 
