a56c922ba249ec4764ad5ecfa79fdb656da0d4cc
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-26 02:15:29 +0700

    Update 2020-04-26 #4 - Ported org-charts 2 and 6.

---
+++ routes/views/10.dev/02.Client/04.org.tree.charts/jsOrgChart-2/routes.js
@@ -0,0 +1,27 @@
+//#region requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+const sfs = require(path.join(rootPath, 'edl', 'server-fs'));
+//const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
+
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+const routes = class {
+    static home(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'index.html');
+    }
+}
+
+router.get('/orgchart/jsorgchart-2', routes.home)
+
+const init_routes = (svr) => {
+    svr.route('/dev', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
