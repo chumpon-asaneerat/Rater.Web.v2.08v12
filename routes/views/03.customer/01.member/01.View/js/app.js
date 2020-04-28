7050b6e4a4104d584cb658d42e7f11899fd7c3d9
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-28 18:27:14 +0700

    Update 2020-04-28 #7 - nlib client add dynamic load js/css helper functions #1.

--- routes/views/03.customer/01.member/01.View/js/app.js
+++ routes/views/03.customer/01.member/01.View/js/app.js
@@ -1,9 +1,34 @@
 let app;
 
-(() => {
+const loadLibrary = (callback) => {
+    let iCnt = 0;
+    let completed = () => {
+        ++iCnt
+        if (iCnt === 2) {
+            if (callback) callback()
+        }
+    }
+
+    nlib.document.load('/dist/css/tabulator.min.css', 'css', completed)
+    nlib.document.load('/dist/js/tabulator.min.js', 'js', completed)
+}
+
+const run = () => {
     let tags = riot.mount('rater-web-app')
     let screenId = 'member-view'
     screens.show(screenId)
+}
+
+(() => {
+    // load css/js files.
+    let loaded = (window.Tabulator !== undefined && window.Tabulator !== null)
+    console.log('Tabulator:', (loaded) ? 'loaded' : 'not loaded')
+    if (!loaded) { 
+        loadLibrary(run)
+    } 
+    else { 
+        run()
+    }
 })();
 /*
 let app;
