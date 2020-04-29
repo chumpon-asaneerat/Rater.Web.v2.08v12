b2837549468ce77a1152898b595c5503178ca924
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-29 21:31:38 +0700

    Update 2020-04-29 #2 nlib client update dynamic load js/css helper functions #7.

--- routes/views/03.customer/01.member/01.View/js/app.js
+++ routes/views/03.customer/01.member/01.View/js/app.js
@@ -1,15 +1,38 @@
 let app;
 (() => {
     // load css/js files.
-    let urls = [
+    let cssFiles = [
         '/dist/css/tabulator.min.css',
-        '/dist/js/tabulator.min.js'
+        '/dist/css/themes/default/style.min.css',
+        '/dist/css/highcharts.css'
     ]
-    nlib.document.load(...urls).then(() => {
-        let tags = riot.mount('rater-web-app')
-        let screenId = 'member-view'
-        screens.show(screenId)
-    });
+    let cssLoaded = false
+    nlib.runtime.css.load(...cssFiles).then(() => {
+        cssLoaded = true
+        console.log('all css loaded')
+        run()
+    })
+
+    let jsFiles = [
+        '/dist/js/tabulator.min.js',
+        '/dist/js/jstree.min.js',
+        '/dist/js/highcharts.js'
+    ]
+    let jsLoaded = false
+    nlib.runtime.js.load(...jsFiles).then(() => {
+        jsLoaded = true
+        console.log('all js loaded')
+        run()
+    })
+
+    let run = () => {
+        if (cssLoaded && jsLoaded) {
+            console.log('mount app')
+            let tags = riot.mount('rater-web-app')
+            let screenId = 'member-view'
+            screens.show(screenId)
+        }
+    }
 })();
 /*
 let app;
