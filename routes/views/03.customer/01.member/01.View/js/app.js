afa049631e93b867781311825b413c093d830cd6
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-29 22:23:39 +0700

    Update 2020-04-29 #3 nlib client update dynamic load js/css helper functions #8.

--- routes/views/03.customer/01.member/01.View/js/app.js
+++ routes/views/03.customer/01.member/01.View/js/app.js
@@ -1,5 +1,23 @@
 let app;
 (() => {
+    let files = [
+        '/dist/css/tabulator.min.css',
+        '/dist/js/tabulator.min.js',
+        '/dist/css/themes/default/style.min.css',
+        '/dist/js/jstree.min.js',
+        '/dist/css/highcharts.css',
+        '/dist/js/highcharts.js'
+    ]
+    nlib.runtime.file.load(...files).then(() => {
+        //console.log('mount app')
+        let tags = riot.mount('rater-web-app')
+        let screenId = 'member-view'
+        screens.show(screenId)
+    })
+})()
+/*
+let app;
+(() => {
     // load css/js files.
     let cssFiles = [
         '/dist/css/tabulator.min.css',
@@ -34,6 +52,7 @@ let app;
         }
     }
 })();
+*/
 /*
 let app;
 (() => {
