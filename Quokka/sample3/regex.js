d10ce0cbc58d954f909f2cd553e10469b20125c5
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-05 06:18:23 +0700

    Update 2020-05-05 #7 - Update quokka sample for rest api regex #4.

--- Quokka/sample3/regex.js
+++ Quokka/sample3/regex.js
@@ -17,42 +17,36 @@ let validUrls = [
     /^\/edl?(\/licenses)/,
     /^\/edl?(\/users(\/\S))/,
     */
-    /*
     // /edl/customers
     /^\/edl?\/customers/,
     // /edl/customers/{customerId}
-    /^\/edl?\/customers?\/edl/,
-    */
-    /*
-    ^    => beginning of string
-    (?!) => negative look-ahead
-    \/?  => 0 or 1 slash
-    .+   => 1 or more character (any except newline) 
-    $    => end of string
-    */
-    /^(?=\/?api).+$/gm
+    /\/edl(\/(customers|users)(\/EDL-\w+(\/(branchs|orgs)(\/(B|O)\w+)*))*)*/gm,
+    /\/api(\/(customers|users)(\/EDL-\w+(\/(branchs|orgs)(\/(B|O)\w+)*))*)*/gm
 ]
 let isMatch = (valids, str) => {
     return valids.some((pattern) => { return pattern.test(str) })
 }
 let str;
 //str = '/edl/customers/EDL-2020050500001/staffs/M0002'
-//str = '/edl/users/edl'
-//str = '/edl/customers'
-//str = '/edl/customers/'
-str = '/api' // valid
-str = '/' // invalid
-str = '/api' // invalid
-let isValid = isMatch(validUrls, str)
+str = '/api' // --> PASS
+set = '/edl/customers' // --> PASS
+set = '/edl/customers/EDL-2020050500003/xxx'
+set = '/edl/customers/EDL-2020050500001/branchs'
+set = '/edl/customers/EDL-2020050500003/branchs/B001'
+set = '/edl/customers/EDL-2020050500003/sample/X001'
+set = '/edl/user'
 
+let isValid = isMatch(validUrls, str)
 console.log('is valid:', isValid)
 
-// /(edl|api)((\/customers(\/\S+(\/(branchs|orgs))))|(\/users))
+// \/(edl|api)(\/(customers|users)(\/EDL-\w+(\/(branchs|orgs)(\/(B|O)\w+)*))*)*
 /*
 /edl/customers
 /edl/customers/EDL-2020050500001/branchs
+/edl/customers/EDL-2020050500003/branchs/B001
 /edl/users
 /edl/customers/EDL-2020050500001/orgs
+/edl/customers/EDL-2020050500004/orgs/O001
 /api
 /api/customers
 */
