4f010ac44f5486e8b016263618a0159e98671f91
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-05 04:38:07 +0700

    Update 2020-05-05 #5 - Update quokka sample for rest api regex #2.

--- Quokka/sample3/regex.js
+++ Quokka/sample3/regex.js
@@ -17,19 +17,32 @@ let validUrls = [
     /^\/edl?(\/licenses)/,
     /^\/edl?(\/users(\/\S))/,
     */
+    /*
     // /edl/customers
-    /^\/edl\/customers/,
+    /^\/edl?\/customers/,
     // /edl/customers/{customerId}
-    /^\/edl\/customers\/edl/,
+    /^\/edl?\/customers?\/edl/,
+    */
+    /*
+    ^    => beginning of string
+    (?!) => negative look-ahead
+    \/?  => 0 or 1 slash
+    .+   => 1 or more character (any except newline) 
+    $    => end of string
+    */
+    /^(?=\/?api).+$/gm
 ]
 let isMatch = (valids, str) => {
     return valids.some((pattern) => { return pattern.test(str) })
 }
 let str;
-str = '/edl/customers/EDL-2020050500001/staffs/M0002'
-str = '/edl/users/edl'
-str = '/edl/customers'
-str = '/edl/customers/EDL-202005050000'
+//str = '/edl/customers/EDL-2020050500001/staffs/M0002'
+//str = '/edl/users/edl'
+//str = '/edl/customers'
+//str = '/edl/customers/'
+str = '/api' // valid
+str = '/' // invalid
+str = '/api' // invalid
 let isValid = isMatch(validUrls, str)
 
 console.log('is valid:', isValid)
