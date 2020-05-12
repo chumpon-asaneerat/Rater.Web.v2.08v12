2c4d1f1f7390910eba49a40e480f0bb7325ee152
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-12 22:24:22 +0700

    Update 2020-05-12 #6 - Fixed report api #4

--- routes/api/03.customer/report/staffcompare/routes.js
+++ routes/api/03.customer/report/staffcompare/routes.js
@@ -76,10 +76,10 @@ api.Get = class {
 
 router.use(secure.checkAccess);
 // routes for staff summaries
-router.all('/', api.Get.entry);
+router.all('/report/staffcompare', api.Get.entry);
 
 const init_routes = (svr) => {
-    svr.route('/customers/api/report/staffcompare/', router);
+    svr.route('/customers/api/', router);
 };
 
 module.exports.init_routes = exports.init_routes = init_routes;
