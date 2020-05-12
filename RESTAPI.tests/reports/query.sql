afbf31cbb454e7c6c53275d35a25cee49dcebe42
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-12 22:32:09 +0700

    Update 2020-05-12 #9 - Add test query for votesummary report and releate question SP.

---
+++ RESTAPI.tests/reports/query.sql
@@ -0,0 +1,15 @@
+EXEC GetVoteSummaries 
+	 NULL
+   , N'EDL-C2020050001'
+   , N'QS00001'
+   , 1
+   , N'2020-05-01'
+   , N'2020-05-31'
+   , N'O0001'
+   , NULL --device id
+   , NULL --user id
+
+EXEC GetQSet N'EN', N'EDL-C2020050001', 'QS00001', 1
+-- update script 2020-05-12
+-- - supports NULL QSeq
+EXEC GetQSlideItems NULL, N'EDL-C2020050001', N'QS00001', NULL, 1
