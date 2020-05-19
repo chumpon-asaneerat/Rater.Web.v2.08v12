eff953b33b4f066b3330097116c312fbb550ea53
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-19 19:20:11 +0700

    Update 2020-05-19 #7 - add dev->fabric.js->es6 sample.

---
+++ routes/views/10.dev/02.Client/02.fabricjs/05 - ES6/routes.js
@@ -0,0 +1,40 @@
+//#region requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+const sfs = require(path.join(rootPath, 'edl', 'server-fs'));
+//const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
+
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+const routes = class {
+    /**
+     * home
+     * @param {Request} req The Request.
+     * @param {Response} res The Response.
+     */
+    static home(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'index.html');
+    }
+    static getCss(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'style.css');
+    }
+    static getJs(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'script.js');
+    }
+}
+
+router.get('/es6', routes.home)
+router.get('/es6/style.css', routes.getCss)
+router.get('/es6/script.js', routes.getJs)
+
+const init_routes = (svr) => {
+    svr.route('/dev/fabricjs', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
---
+++ routes/views/10.dev/02.Client/02.fabricjs/05 - ES6/script.js
@@ -0,0 +1,138 @@
+/*
+function createClass() {
+    var parent = null, properties = slice.call(arguments, 0);
+
+    if (typeof properties[0] === 'function') { parent = properties.shift(); }
+    function klass() { this.initialize.apply(this, arguments); }
+
+    klass.superclass = parent;
+    klass.subclasses = [];
+
+    if (parent) {
+      Subclass.prototype = parent.prototype;
+      klass.prototype = new Subclass();
+      parent.subclasses.push(klass);
+    }
+    for (var i = 0, length = properties.length; i < length; i++) {
+      addMethods(klass, properties[i], parent);
+    }
+    if (!klass.prototype.initialize) {
+      klass.prototype.initialize = emptyFunction;
+    }
+    klass.prototype.constructor = klass;
+    klass.prototype.callSuper = callSuper;
+    return klass;
+  }
+*/
+var slice = Array.prototype.slice, push = Array.prototype.push;
+function emptyFunction() {}
+function Subclass() { }
+addMethods = function(klass, source, parent) {
+    for (var property in source) {
+      if (property in klass.prototype &&
+          typeof klass.prototype[property] === 'function' &&
+          (source[property] + '').indexOf('callSuper') > -1) {
+        klass.prototype[property] = (function(property) {
+          return function() {
+            var superclass = this.constructor.superclass;
+            this.constructor.superclass = parent;
+            var returnValue = source[property].apply(this, arguments);
+            this.constructor.superclass = superclass;
+            if (property !== 'initialize') {
+              return returnValue;
+            }
+          };
+        })(property);
+      }
+      else {
+        klass.prototype[property] = source[property];
+      }
+
+      //if (IS_DONTENUM_BUGGY) {
+        if (source.toString !== Object.prototype.toString) {
+          klass.prototype.toString = source.toString;
+        }
+        if (source.valueOf !== Object.prototype.valueOf) {
+          klass.prototype.valueOf = source.valueOf;
+        }
+      //}
+    }
+}
+function callSuper(methodName) {
+    var parentMethod = null,
+        _this = this;
+
+    // climb prototype chain to find method not equal to callee's method
+    while (_this.constructor.superclass) {
+      var superClassMethod = _this.constructor.superclass.prototype[methodName];
+      if (_this[methodName] !== superClassMethod) {
+        parentMethod = superClassMethod;
+        break;
+      }
+      // eslint-disable-next-line
+      _this = _this.constructor.superclass.prototype;
+    }
+
+    if (!parentMethod) {
+      return console.log('tried to callSuper ' + methodName + ', method not found in prototype chain', this);
+    }
+
+    return (arguments.length > 1)
+      ? parentMethod.apply(this, slice.call(arguments, 1))
+      : parentMethod.call(this);
+  }
+
+function createClass() {
+    var parent = null, properties = slice.call(arguments, 0);
+    if (typeof properties[0] === 'function') { parent = properties.shift(); }
+    function klass() { 
+        // constructor.
+        this.initialize.apply(this, arguments);
+    }
+    klass.superclass = parent;
+    klass.subclasses = [];
+    if (parent) {
+        Subclass.prototype = parent.prototype;
+        klass.prototype = new Subclass();
+        parent.subclasses.push(klass);
+    }
+    for (var i = 0, length = properties.length; i < length; i++) {
+        addMethods(klass, properties[i], parent);
+    }
+    if (!klass.prototype.initialize) {
+        klass.prototype.initialize = emptyFunction;
+    }
+    klass.prototype.constructor = klass;
+    klass.prototype.callSuper = callSuper;
+    return klass;
+}
+
+(() => {
+    console.log('script.js loaded.')
+    let obj = {
+        type: 'LabeledRect',
+        initialize: function(options) {
+        },
+        toObject: function() { 
+        },
+        _render: function(ctx) {
+        }
+    }
+    console.log(obj)
+
+    let Base = createClass(null, {})
+    console.log('Base:', Base)
+    let Rect = createClass(Base, { type:'Rect' })
+    console.log('Rect:', Rect)
+
+    class es6 {
+        constructor() {
+            this.type = 'LabeledRect'
+        }
+        initialize(options) { }
+        toObject() { }
+        _render(ctx) { }
+    }
+
+    console.log(es6.prototype)
+})();
