5f9e2de547d81254ac0c2dc4e0809f4937876295
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-24 23:49:46 +0700

    Update 2020-05-24 #7 - Update route code for dev->js/assets and dev->js/assets2. - add app.js and script.js for dev->js/assets and dev->js/assets2.

--- routes/views/10.dev/02.Client/00.js/02.assets2/routes.js
+++ routes/views/10.dev/02.Client/00.js/02.assets2/routes.js
@@ -14,11 +14,21 @@ const router = new WebRouter();
 
 const routes = class {
     static home(req, res) {
-        WebServer.sendFile(req, res, __dirname, 'index.html');
+        WebServer.sendFile(req, res, __dirname, 'html', 'index.html');
+    }
+    static getjsfile(req, res) {
+        let file = req.params.file.toLowerCase();
+        let files = ['app.js', 'script.js']
+        let idx = files.indexOf(file);
+        if (idx !== -1) {
+            let fname = path.join(__dirname, 'js', files[idx]);
+            WebServer.sendFile(req, res, fname);
+        }
     }
 }
 
 router.get('/', routes.home)
+router.get('/js/:file', routes.getjsfile)
 
 const init_routes = (svr) => {
     svr.route('/dev/js/assets2', router);
