f19c3089128b29cb678c0f364d38586882274528
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-26 19:31:29 +0700

    Update 2020-05-26 #10 - Add riot tag for common components #37. - Update dev->riotjs->mount.and.trigger sample #7.

--- routes/views/10.dev/02.Client/03.riotjs/17.mount.and.trigger/js/app.js
+++ routes/views/10.dev/02.Client/03.riotjs/17.mount.and.trigger/js/app.js
@@ -1,45 +1,22 @@
 let app
 
 const eventbus = function() {
-    console.log('init event bus.')
+    //console.log('init event bus.')
     riot.observable(this)
 }
-
+riot.tagCount = 0
+riot.eventbus = new eventbus()
+riot.eventbus.on('all-tags-mounted', () => { console.log('all tags mounted') })
 riot.hook = (tag) => {
-    tag.on('before-mount', () => { 
-        //console.log(tag.__.tagName, ' before-mount.')
-        //riot.eventbus.trigger('update-tag-count')
-        riot.tagCount++
-    })
+    tag.on('before-mount', () => { riot.tagCount++ })
     tag.on('mount', () => {
-        //console.log(tag.__.tagName, ' mounted. group: ', tag.opts.group)
-        //riot.eventbus.trigger('update-tag-mount')
         riot.tagCount--
         if (riot.tagCount <= 0) {
             riot.eventbus.trigger('all-tags-mounted')
         }
         })
-    tag.on('unmount', () => {
-        console.log(tag.__.tagName, ' unmount')
-    })
+    tag.on('unmount', () => {})
 }
-
-riot.tagCount = 0
-riot.eventbus = new eventbus()
-riot.eventbus.on('all-tags-mounted', () => {
-    console.log('all tags mounted')
-})
-/*
-riot.eventbus.on('update-tag-count', () => {
-    riot.tagCount++
-})
-riot.eventbus.on('update-tag-mount', () => {
-    riot.tagCount--
-    if (riot.tagCount <= 0) {
-        riot.eventbus.trigger('all-tags-mounted')
-    }
-})
-*/
 
 ;(() => {
     app = riot.mount('rater-web-app')
