fa67b5926ffd809e98ef146a674f8b865353a68d
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-28 04:07:41 +0700

    Update 2020-05-28 #6 - Ported autofill5 sample.

---
+++ routes/views/10.dev/02.Client/00.js/06.AutoFillv5/routes.js
@@ -0,0 +1,52 @@
+//#region requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+const sfs = require(path.join(rootPath, 'edl', 'server-fs'));
+//const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
+
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+const routes = class {
+    /**
+     * home
+     * @param {Request} req The Request.
+     * @param {Response} res The Response.
+     */
+    static home(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'html', 'index.html');
+    }
+    static getcssfile(req, res) {
+        let file = req.params.file.toLowerCase();
+        let files = ['style.css', 'nlib.css']
+        let idx = files.indexOf(file);
+        if (idx !== -1) {
+            let fname = path.join(__dirname, 'css', files[idx]);
+            WebServer.sendFile(req, res, fname);
+        }
+    }
+    static getjsfile(req, res) {
+        let file = req.params.file.toLowerCase();
+        let files = ['app.js', 'nlib.js']
+        let idx = files.indexOf(file);
+        if (idx !== -1) {
+            let fname = path.join(__dirname, 'js', files[idx]);
+            WebServer.sendFile(req, res, fname);
+        }
+    }
+}
+
+router.get('/', routes.home)
+router.get('/css/:file', routes.getcssfile)
+router.get('/js/:file', routes.getjsfile)
+
+const init_routes = (svr) => {
+    svr.route('/dev/js/autofill5', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
